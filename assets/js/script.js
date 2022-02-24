054a33612ce916f7aa2b2c76c92651bf30ea6abc
ancs214 <ancs214@gmail.com>
2022-02-23 19:37:07 -0500

    refactor code to be more readable/manageable

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,31 +24,41 @@ let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-let createTaskHandler = function (event) {
+let taskFormHandler = function (event) {
     //prevent browser"s old habits of reloading page after form submit
     event.preventDefault();
     //create var and query select the element (<input>) with the name attribute of "task-name"
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     //create var and select for element <select> name attribute
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //package up data as an object
+    let taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput,
+    };
+
+    //send as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+let createTaskEl = function(taskDataObj) {
     //declare DOM var and create a <li> element with same styling
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    //create div to hold task info and add to list item and give it a class name
-    let taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    //add HTML content to taskInfoEl div. add div element to li element
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-    listItemEl.appendChild(taskInfoEl);
-    
-    //add list item to unordered list
+     //create div to hold task info and add to list item and give it a class name
+     let taskInfoEl = document.createElement("div");
+     taskInfoEl.className = "task-info";
+     //add HTML content to taskInfoEl div. add div element to li element
+     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+     listItemEl.appendChild(taskInfoEl);
+     //add list item to unordered list
     tasksToDoEl.appendChild(listItemEl);
-};
-
+}
 
 // EVENT LISTENER has two arguments passed into it: type of event and event response to execute
 //addEventListener to button: when we click, a function will run
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
 
 
 
